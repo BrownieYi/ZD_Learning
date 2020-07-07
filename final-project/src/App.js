1b8d3ff293dec6872c4dd84ba625e2474a759f3d
Mingyu Xie <mingyuxie@ucsb.edu>
2020-07-07 22:34:50 +0800

    added cesium

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,16 +1,27 @@
+/* eslint-disable no-return-assign */
 import React, { Component } from 'react';
+import Viewer from 'cesium/Source/Widgets/Viewer/Viewer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Appbar from './components/Appbar';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.viewer = new Viewer(this.cesiumContainer);
+  }
+
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
+      <div>
       <React.Fragment>
         <CssBaseline />
         <Appbar />
       </React.Fragment>
+      <React.Fragment>
+        <div id="cesiumContainer" ref={ (element) => this.cesiumContainer = element }/>
+      </React.Fragment>
+      </div>
     );
   }
 }
--- final-project/src/index.js
+++ final-project/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import buildModuleUrl from 'cesium/Source/Core/buildModuleUrl';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'cesium/Source/Widgets/widgets.css';
 /* global document */
+
+buildModuleUrl.setBaseUrl('./cesium/');
 
 ReactDOM.render(
     <App />,
@@ -13,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
